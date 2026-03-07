0bc1fa3c69381f2b1c2183c20f4693264b0a5940
Thorleif Jacobsen <>
2026-03-07 10:18:42 +0100

    Prisma changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	outputFileTracingRoot: path.join(import.meta.dirname),
+};
 
 export default nextConfig;
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig, env } from 'prisma/config';
+import 'dotenv/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
-  },
-  datasource: {
-    url: env('DATABASE_URL'),
-    shadowDatabaseUrl: env('SHADOW_DATABASE_URL'),
   },
 });
